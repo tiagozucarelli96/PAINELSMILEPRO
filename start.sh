32c5ab1b663fe311a85239fafbffc996f9aad059
tiagozucarelli96 <tiagozucarellii@icloud.com>
2025-08-18 21:40:44 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ set -e
 
 PORT="${PORT:-8080}"
 
-# Gera a configuração do Nginx usando o PORT fornecido pela Railway
+# Gera config do Nginx atrelada ao $PORT
 cat > /etc/nginx/conf.d/default.conf <<EOF
 server {
     listen ${PORT};
@@ -17,18 +17,16 @@ server {
     }
 
     location ~ \.php$ {
-        include fastcgi_params;
+        include /etc/nginx/fastcgi_params;
         fastcgi_pass 127.0.0.1:9000;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
         fastcgi_read_timeout 300;
-        fastcgi_buffers 16 16k;
-        fastcgi_buffer_size 32k;
     }
 
     client_max_body_size 25M;
 }
 EOF
 
-# Sobe o PHP-FPM em background e o Nginx em foreground
+# Sobe serviços
 php-fpm -D
 nginx -g "daemon off;"
