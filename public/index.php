b2dd1a574d3c9cebcb4e547af17f74fa75890b0f
tiagozucarelli96 <tiagozucarellii@icloud.com>
2025-11-01 04:17:23 +0000

    fix: Adicionar test_asaas_checkout às páginas públicas para permitir acesso via index.php

--- public/index.php
+++ public/index.php
@@ -156,7 +156,7 @@ $routes = [
 ];
 
 /* exige login - EXCETO para páginas públicas */
-$public_pages = ['comercial_degust_public', 'asaas_webhook', 'webhook_me_eventos'];
+$public_pages = ['comercial_degust_public', 'asaas_webhook', 'webhook_me_eventos', 'test_asaas_checkout'];
 $is_public_page = in_array($page, $public_pages);
 
 if (empty($_SESSION['logado']) && !$is_public_page) {
