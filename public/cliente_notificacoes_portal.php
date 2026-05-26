28fdbff9318f486b226719bc049cf877607b8cad
tiagozucarelli96 <tiagozucarellii@icloud.com>
2026-05-26 14:59:41 +0400

    Force portal notification model id

--- public/cliente_notificacoes_portal.php
+++ public/cliente_notificacoes_portal.php
@@ -3,12 +3,16 @@
  * Rota dedicada para o modelo de lançamento do Portal do Cliente.
  */
 $_GET['page'] = 'cliente_notificacoes';
+$_GET['modelo'] = '26';
 $_GET['modelo_chave'] = 'portal_cliente_lancamento';
+$_REQUEST['page'] = 'cliente_notificacoes';
+$_REQUEST['modelo'] = '26';
+$_REQUEST['modelo_chave'] = 'portal_cliente_lancamento';
 
 $queryParams = [];
 parse_str((string)($_SERVER['QUERY_STRING'] ?? ''), $queryParams);
+$queryParams['modelo'] = '26';
 $queryParams['modelo_chave'] = 'portal_cliente_lancamento';
 $_SERVER['QUERY_STRING'] = http_build_query($queryParams);
 
 require __DIR__ . '/cliente_notificacoes.php';
-
